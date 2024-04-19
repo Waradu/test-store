7f7d859ef035593d83b20ede23e5fbcc4ef12d4c
Waradu <waradu@outlook.com>
2024-04-19 15:49:32 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ counter.count }}
+    <button class="click" @click="counter.increment()">Click</button>
+  </div>
+</template>
+
+<script setup>
+const counter = useCounterStore()
+</script>
